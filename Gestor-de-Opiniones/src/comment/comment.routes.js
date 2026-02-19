3856f99adf23a65fb4cab7b0bb3d660955b780b6
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-02-19 17:48:54 -0600

    Feat: se modificaron las rutas para los endpoints y se agrego el route.get para listar todos los comentarios y requiere role admin

--- Gestor-de-Opiniones/src/comment/comment.routes.js
+++ Gestor-de-Opiniones/src/comment/comment.routes.js
@@ -1,23 +1,35 @@
 import { Router } from "express";
-import {createComment, updateComment, deleteComment, getComments, getCommentById } from "./comment.controller.js";
-import { validateCreateComment, validateUpdateComment, validateCommentById, validateGetComments } from "../../middlewares/comment-validators.js";
+import {createComment, updateComment, deleteComment, getComments, getCommentById, getAllComments } from "./comment.controller.js";
+import {validateCreateComment, validateUpdateComment, validateCommentById, validateGetComments } from "../../middlewares/comment-validators.js";
+import { requireRole } from "../../middlewares/validate-role.js";
+import { validateJWT } from "../../middlewares/validate-JWT.js";
 
 const router = Router();
 
-// crear comentario para una publicación (postId en params)
+// crear comentario para una publicación
 router.post(
-    '/:postId/create',
+    '/post/:postId',
     validateCreateComment,
     createComment
 );
 
-// listar comentarios de una publicación: /post/:postId
+// ver los comentarios de un post
 router.get(
     '/post/:postId',
     validateGetComments,
     getComments
 );
 
+// obtener coemntario por id
+
+router.get(
+    '/',
+    validateJWT,
+    requireRole('ADMIN_ROLE'),
+    getAllComments
+);
+
+// buscar por id
 router.get(
     '/:id',
     validateCommentById,
